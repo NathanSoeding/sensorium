6fa448b23251eb583958305ca05d874a67f0708f
NathanSoeding <nathanpaul.soeding@stud.uni-goettingen.de>
2025-03-20 18:17:36 +0100

    added assert for shifter and perspective

---
+++ perspective/sensorium2020_adapted.py
@@ -0,0 +1,282 @@
+import warnings
+
+import numpy as np
+import torch
+from torch import nn
+
+from neuralpredictors.layers import activations
+from neuralpredictors.layers.encoders.base import Encoder
+
+from nnfabrik.utility.nn_helpers import set_random_seed, get_dims_for_loader_dict
+from neuralpredictors.utils import get_module_output
+from neuralpredictors.layers.shifters import MLPShifter, StaticAffine2dShifter
+from neuralpredictors.layers.cores import (
+    Stacked2dCore,
+    SE2dCore,
+    RotationEquivariant2dCore,
+)
+
+from sensorium.models.readouts import MultipleFullGaussian2d
+from sensorium.models.utility import prepare_grid
+
+from perspective import Retina, Monitor, Perspective
+
+
+class FiringRateEncoder(Encoder):
+    def __init__(
+        self,
+        core,
+        readout,
+        *,
+        perspective=None,
+        shifter=None,
+        modulator=None,
+        elu_offset=0.0,
+        nonlinearity_type="elu",
+        nonlinearity_config=None
+    ):
+        """
+        An Encoder that wraps the core, readout and optionally a shifter amd modulator into one model.
+        The output is one positive value that can be interpreted as a firing rate, for example for a Poisson distribution.
+        Args:
+            core (nn.Module): Core model. Refer to neuralpredictors.layers.cores
+            readout (nn.ModuleDict): MultiReadout model. Refer to neuralpredictors.layers.readouts
+            elu_offset (float): Offset value in the final elu non-linearity. Defaults to 0.
+            shifter (optional[nn.ModuleDict]): Shifter network. Refer to neuralpredictors.layers.shifters. Defaults to None.
+            modulator (optional[nn.ModuleDict]): Modulator network. Modulator networks are not implemented atm (24/06/2021). Defaults to None.
+            nonlinearity (str): Non-linearity type to use. Defaults to 'elu'.
+            nonlinearity_config (optional[dict]): Non-linearity configuration. Defaults to None.
+        """
+        super().__init__()
+        self.core = core
+        self.readout = readout
+        self.perspective=perspective
+        self.shifter = shifter
+        self.modulator = modulator
+        self.offset = elu_offset
+
+        if nonlinearity_type != "elu" and not np.isclose(elu_offset, 0.0):
+            warnings.warn("If `nonlinearity_type` is not 'elu', `elu_offset` will be ignored")
+        if nonlinearity_type == "elu":
+            self.nonlinearity_fn = nn.ELU()
+        elif nonlinearity_type == "identity":
+            self.nonlinearity_fn = nn.Identity()
+        else:
+            self.nonlinearity_fn = activations.__dict__[nonlinearity_type](
+                **nonlinearity_config if nonlinearity_config else {}
+            )
+        self.nonlinearity_type = nonlinearity_type
+
+    def forward(
+        self,
+        inputs,
+        *args,
+        targets=None,
+        data_key=None,
+        behavior=None,
+        pupil_center=None,
+        trial_idx=None,
+        shift=None,
+        detach_core=False,
+        **kwargs
+    ):
+        if self.perspective:
+            # if shift is defined - no need to change it
+            if pupil_center is None:
+                raise ValueError("pupil_center is not given")
+            x = self.perspective(inputs, pupil_center)
+            self.shifter=None
+        else:
+            x = inputs
+
+        x = self.core(x)
+        if detach_core:
+            x = x.detach()
+
+        if self.shifter and shift is None:
+            # if shift is defined - no need to change it
+            if pupil_center is None:
+                raise ValueError("pupil_center is not given")
+            shift = self.shifter[data_key](pupil_center, trial_idx)
+
+        x = self.readout(x, data_key=data_key, shift=shift, **kwargs)
+        x = x[None, ...] if len(x.shape) == 1 else x  # keep dimensions if only one image was passed
+
+        if self.modulator:
+            if behavior is None:
+                raise ValueError("behavior is not given")
+            x = self.modulator[data_key](x, behavior=behavior)
+
+        if self.nonlinearity_type == "elu":
+            return self.nonlinearity_fn(x + self.offset) + 1
+        else:
+            return self.nonlinearity_fn(x)
+
+    def predict_mean(self, x, *args, data_key=None, **kwargs):
+        return self.forward(x, *args, data_key=data_key, **kwargs)
+
+    def predict_variance(self, x, *args, data_key=None, **kwargs):
+        return self.forward(x, *args, data_key=data_key, **kwargs)
+
+def stacked_core_full_gauss_readout(
+    dataloaders,
+    seed,
+    hidden_channels=32,
+    input_kern=13,
+    hidden_kern=3,
+    layers=3,
+    gamma_input=15.5,
+    skip=0,
+    final_nonlinearity=True,
+    momentum=0.9,
+    pad_input=False,
+    batch_norm=True,
+    hidden_dilation=1,
+    laplace_padding=None,
+    input_regularizer="LaplaceL2norm",
+    use_avg_reg=False,
+    init_mu_range=0.2,
+    init_sigma=1.0,
+    readout_bias=True,
+    gamma_readout=4,
+    elu_offset=0,
+    stack=None,
+    depth_separable=False,
+    linear=False,
+    gauss_type="full",
+    grid_mean_predictor=None,
+    attention_conv=False,
+    shifter=None,
+    shifter_type="MLP",
+    input_channels_shifter=2,
+    hidden_channels_shifter=5,
+    shift_layers=3,
+    gamma_shifter=0,
+    shifter_bias=True,
+    perspective=None,
+    hidden_padding=None,
+    core_bias=True,
+):
+    """
+    Model class of a stacked2dCore (from neuralpredictors) and a pointpooled (spatial transformer) readout
+
+    Args:
+        dataloaders: a dictionary of dataloaders, one loader per session
+            in the format {'data_key': dataloader object, .. }
+        seed: random seed
+        grid_mean_predictor: if not None, needs to be a dictionary of the form
+            {
+            'type': 'cortex',
+            'input_dimensions': 2,
+            'hidden_layers':0,
+            'hidden_features':20,
+            'final_tanh': False,
+            }
+            In that case the datasets need to have the property `neurons.cell_motor_coordinates`
+        share_features: whether to share features between readouts. This requires that the datasets
+            have the properties `neurons.multi_match_id` which are used for matching. Every dataset
+            has to have all these ids and cannot have any more.
+        all other args: See Documentation of Stacked2dCore in neuralpredictors.layers.cores and
+            PointPooled2D in neuralpredictors.layers.readouts
+
+    Returns: An initialized model which consists of model.core and model.readout
+    """
+
+    if "train" in dataloaders.keys():
+        dataloaders = dataloaders["train"]
+
+    # Obtain the named tuple fields from the first entry of the first dataloader in the dictionary
+    batch = next(iter(list(dataloaders.values())[0]))
+    in_name, out_name = (
+        list(batch.keys())[:2] if isinstance(batch, dict) else batch._fields[:2]
+    )
+
+    session_shape_dict = get_dims_for_loader_dict(dataloaders)
+    n_neurons_dict = {k: v[out_name][1] for k, v in session_shape_dict.items()}
+    input_channels = [v[in_name][1] for v in session_shape_dict.values()]
+
+    core_input_channels = (
+        list(input_channels.values())[0]
+        if isinstance(input_channels, dict)
+        else input_channels[0]
+    )
+
+    set_random_seed(seed)
+    grid_mean_predictor, grid_mean_predictor_type, source_grids = prepare_grid(grid_mean_predictor, dataloaders)
+
+    core = Stacked2dCore(
+        input_channels=core_input_channels,
+        hidden_channels=hidden_channels,
+        input_kern=input_kern,
+        hidden_kern=hidden_kern,
+        layers=layers,
+        gamma_input=gamma_input,
+        skip=skip,
+        final_nonlinearity=final_nonlinearity,
+        bias=core_bias,
+        momentum=momentum,
+        pad_input=pad_input,
+        batch_norm=batch_norm,
+        hidden_dilation=hidden_dilation,
+        laplace_padding=laplace_padding,
+        input_regularizer=input_regularizer,
+        stack=stack,
+        depth_separable=depth_separable,
+        linear=linear,
+        attention_conv=attention_conv,
+        hidden_padding=hidden_padding,
+        use_avg_reg=use_avg_reg,
+    )
+
+    in_shapes_dict = {
+        k: get_module_output(core, v[in_name])[1:]
+        for k, v in session_shape_dict.items()
+    }
+
+    readout = MultipleFullGaussian2d(
+        in_shape_dict=in_shapes_dict,
+        loader=dataloaders,
+        n_neurons_dict=n_neurons_dict,
+        init_mu_range=init_mu_range,
+        bias=readout_bias,
+        init_sigma=init_sigma,
+        gamma_readout=gamma_readout,
+        gauss_type=gauss_type,
+        grid_mean_predictor=grid_mean_predictor,
+        grid_mean_predictor_type=grid_mean_predictor_type,
+        source_grids=source_grids,
+    )
+
+    assert (perspective and shifter) == False
+
+    if shifter is True:
+        data_keys = [i for i in dataloaders.keys()]
+        if shifter_type == "MLP":
+            shifter = MLPShifter(
+                data_keys=data_keys,
+                input_channels=input_channels_shifter,
+                hidden_channels_shifter=hidden_channels_shifter,
+                shift_layers=shift_layers,
+                gamma_shifter=gamma_shifter,
+            )
+
+        elif shifter_type == "StaticAffine":
+            shifter = StaticAffine2dShifter(
+                data_keys=data_keys,
+                input_channels=input_channels_shifter,
+                bias=shifter_bias,
+                gamma_shifter=gamma_shifter,
+            )
+
+    if perspective is True:
+        perspective = Perspective(Retina(), Monitor())
+
+    model = FiringRateEncoder(
+        core=core,
+        readout=readout,
+        perspective=perspective,
+        shifter=shifter,
+        elu_offset=elu_offset,
+    )
+
+    return model

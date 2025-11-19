ad9ea8722f392eb4151a99f4c947e789c3367531
NathanSoeding <nathanpaul.soeding@stud.uni-goettingen.de>
2025-11-19 17:11:21 +0100

    autoencoder removed variational part

--- small_readout_vectors/autoencoder.py
+++ small_readout_vectors/autoencoder.py
@@ -1,9 +1,9 @@
 import torch
 from torch import nn
 
-class VAE(nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dims=[32, 16], batch_norm=True, dropout=0.1, nonlinearity='ReLU', leakyness=0.2):
-        super(VAE, self).__init__()
+class Autoenc(nn.Module):
+    def __init__(self, input_dim, latent_dim, hidden_dims=[32, 16], batch_norm=False, dropout=0.0, nonlinearity='ReLU', leakyness=0.2):
+        super(Autoenc, self).__init__()
         
         if nonlinearity == 'ReLU':
             transfer_fn = nn.ReLU()
@@ -18,14 +18,11 @@ class VAE(nn.Module):
         for h_dim in hidden_dims:
             encoder_layers.append(nn.Linear(prev_dim, h_dim))
             encoder_layers.append(nn.BatchNorm1d(h_dim)) if batch_norm else None
-            encoder_layers.append(nn.ReLU())
-            encoder_layers.append(nn.Dropout(dropout))
+            encoder_layers.append(transfer_fn)
+            encoder_layers.append(nn.Dropout(dropout)) if dropout > 0.0 else None
             prev_dim = h_dim
+        encoder_layers.append(nn.Linear(prev_dim, latent_dim))
         self.encoder = nn.Sequential(*encoder_layers)
-        
-        # Latent space
-        self.mu = nn.Linear(prev_dim, latent_dim)
-        self.logvar = nn.Linear(prev_dim, latent_dim)
         
         # Decoder
         decoder_layers = []
@@ -33,8 +30,8 @@ class VAE(nn.Module):
         for h_dim in reversed(hidden_dims):
             decoder_layers.append(nn.Linear(prev_dim, h_dim))
             decoder_layers.append(nn.BatchNorm1d(h_dim)) if batch_norm else None
-            decoder_layers.append(nn.ReLU())
-            encoder_layers.append(nn.Dropout(dropout))
+            decoder_layers.append(transfer_fn)
+            decoder_layers.append(nn.Dropout(dropout)) if dropout > 0.0 else None
             prev_dim = h_dim
         decoder_layers.append(nn.Linear(prev_dim, input_dim))
         self.decoder = nn.Sequential(*decoder_layers)
@@ -54,7 +51,7 @@ class VAE(nn.Module):
         return self.decoder(z)
     
     def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        recon_x = self.decode(z)
-        return recon_x, mu, logvar
+        z = self.encoder(x)
+        recon = self.decoder(z)
+        
+        return recon

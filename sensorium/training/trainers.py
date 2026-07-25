last fixes for kl loss

sensorium/training/trainers.py
@@ -121,6 +121,16 @@ def standard_trainer(
         weight = (batch**exponent) / torch.sum(batch, 0)
         return (weight.t() / torch.sum(weight, 1)).t()
 
+    def _canonicalize_features(features, outdims):
+        """
+        Nina's DEC-clustering code assumes readout.features (after squeeze) is (channels,
+        outdims) -- true for FullGaussian2d but not Factorized2d, whose features are stored
+        (outdims, channels). Returns features as (outdims, channels) regardless of the
+        underlying readout's storage convention.
+        """
+        features = features.squeeze()
+        return features if features.shape[0] == outdims else features.T
+
     def soft_assignments_mult(encoded_features, cluster_centers, sigma, alpha, p=1, mixing_coefficients=None):
         """Calculates the q_ij as the t mixture components. Moves to log space to avoid numerical issues."""
         sigma_inv = 1.0 / sigma  # (K, D)
@@ -319,7 +329,7 @@ def standard_trainer(
             # form initial cluster centres
             with torch.no_grad():
                 for i,(k,readout) in enumerate(model.readout.items()):
-                    features = readout.features.cpu().detach().squeeze().T.numpy()
+                    features = _canonicalize_features(readout.features.cpu().detach(), readout.outdims).numpy()
                     feature_list.append(np.array(features))
 
                 features = np.vstack(feature_list)
@@ -391,7 +401,7 @@ def standard_trainer(
                     kldiv_loss = torch.zeros(1).to(device)
                     feature_list = []
                     for i, (k, readout) in enumerate(model.readout.items()):
-                        features = readout.features.squeeze()
+                        features = _canonicalize_features(readout.features, readout.outdims).T
                         feature_list.append(features)
 
                     # features_subset = torch.cat(features_subset, dim=1)
@@ -481,7 +491,7 @@ def standard_trainer(
     if include_kldivergence:
         soft_assignments_list = []
         for i,(k, readout) in enumerate(model.readout.items()):
-            features = readout.features.detach().squeeze()
+            features = _canonicalize_features(readout.features.detach(), readout.outdims).T
             if include_mixingcoefficients:
                 soft_assignments_list.append(
                     soft_assignments_mult(features, cluster_centers, sigma, alpha, p, mixing_coefficients)

training/train.py
@@ -7,6 +7,7 @@ from sensorium.utility import get_data, set_random_seed
 from sensorium.models import stacked_core_full_gauss_readout, stacked_core_factorized_readout
 from sensorium.models.zig_model import stacked_core_zig_gauss_readout
 from sensorium.training import standard_trainer
+import pickle
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -64,22 +65,30 @@ def main():
 
     basepath = "/srv/user/polina/sensorium/sensorium/notebooks/data/"
     # as filenames, we'll select all 7 datasets
+    # filenames = [
+    #     os.path.join(basepath, file) for file in os.listdir(basepath) if ".zip" in file
+    # ]
+
     filenames = [
-        os.path.join(basepath, file) for file in os.listdir(basepath) if ".zip" in file
+        os.path.join(basepath, file) for file in os.listdir(basepath) if ".zip" in file and '26872-17-20' not in file
     ]
 
     if args.more_data:
         more_basepath = "/user/turishcheva/more_data_like_sensorium_2022"
+        # we should exclude mouse 20892 since it has not only V1 but also other areas!
+        # We also excluded mouse 20622 since it has data from L4 and not L2/3 as all the other mice
         for file in [
             "static20457-5-9-94c6ff995dac583098847cfecd43e7b6",
-            "static20622-2-14-94c6ff995dac583098847cfecd43e7b6",
-            "static20892-10-10-94c6ff995dac583098847cfecd43e7b6",
+            # "static20622-2-14-94c6ff995dac583098847cfecd43e7b6",
+            # "static20892-10-10-94c6ff995dac583098847cfecd43e7b6",
             "static22223-2-15-94c6ff995dac583098847cfecd43e7b6",
             "static22564-2-12-94c6ff995dac583098847cfecd43e7b6",
             "static22620-4-15-94c6ff995dac583098847cfecd43e7b6",
             "static23555-5-12-94c6ff995dac583098847cfecd43e7b6",
         ]:
             filenames.append(os.path.join(more_basepath, file))
+
+    print(len(filenames), filenames)
 
     dataset_fn = "sensorium.datasets.static_loaders"
     dataset_config = {
@@ -193,7 +202,7 @@ def main():
         **trainer_config
     )
     torch.save(model.state_dict(), f'{args.output_dir}/model_weights.pth')
-    with open(f'{args.output_dir}output_dict.pkl', 'wb') as f:
+    with open(f'{args.output_dir}/output_dict.pkl', 'wb') as f:
         pickle.dump(trainer_output, f)
 
     # Save everything needed to reconstruct the model